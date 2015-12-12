b59ec95d7617568fe97f2672bde5711ae49dc3a4
Tropic <tropical.productions.scratch@gmail.com>
2015-12-12 19:42:30 +0100

    Added mall for

--- BuildToCmd.py
+++ BuildToCmd.py
@@ -1,1 +1,25 @@
+displayName = "Test Filter"
 
+inputs = (
+    ("Example 1", (-1, 0, 1)),
+    ("Example 2", "test"),
+    ("Example 3", ("str1", "str2", "str3")),
+)
+
+def perform(level, box, options):
+    if options["Example 1"]:
+        # do something
+
+    varStor = options["Example 2"] # Storing option value in variable
+    
+    if method == "Use blockAt":
+        for x in xrange(box.minx, box.maxx):
+            for z in xrange(box.minz, box.maxz):
+                for y in xrange(box.miny, box.maxy): # Selection box
+                    if level.blockAt(x, y, z) == 0: # Tests for block using ID
+                        level.setBlockAt(x, y, z, 1) # Places block using ID
+    
+    if varStor == "something":
+        # do something
+    
+    pos = level.getPlayerPosition() # Get player position and asign it to a variable
