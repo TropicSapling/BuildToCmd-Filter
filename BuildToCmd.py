43abbf3bd082cc66e436fe4289b7b173da771239
Tropic <tropical.productions.scratch@gmail.com>
2015-12-12 19:45:39 +0100

    Update BuildToCmd.py

--- BuildToCmd.py
+++ BuildToCmd.py
@@ -7,19 +7,19 @@ inputs = (
 )
 
 def perform(level, box, options):
-    if options["Example 1"]:
+    if options["Example 1"]: # Test for if option has been filled in
         # do something
 
     varStor = options["Example 2"] # Storing option value in variable
     
-    if method == "Use blockAt":
+    if varStor == "testing": # Test for if option value is 'testing'
         for x in xrange(box.minx, box.maxx):
             for z in xrange(box.minz, box.maxz):
                 for y in xrange(box.miny, box.maxy): # Selection box
                     if level.blockAt(x, y, z) == 0: # Tests for block using ID
                         level.setBlockAt(x, y, z, 1) # Places block using ID
     
-    if varStor == "something":
+    if varStor == "something": # Test for if option value is 'something'
         # do something
     
     pos = level.getPlayerPosition() # Get player position and asign it to a variable
