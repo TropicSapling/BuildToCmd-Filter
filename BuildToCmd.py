50621c35abb37ab0ed1a15ee67cdc8f76837aebc
Tropic <tropical.productions.scratch@gmail.com>
2015-12-13 13:30:51 +0100

    Added basic stuff
    Basic inputs, if statements and for loops have been added.
    What's left to add is generating the cmd :)

--- BuildToCmd.py
+++ BuildToCmd.py
@@ -1,14 +1,35 @@
 displayName = "Test Filter"
 
 inputs = (
+    ("Export to CMD", "title"),
     ("Move", False),
     ("Masked", False),
-    ("Filtered", (False, 0)),
+    ("Filtered", False),
+    ("IF Filtered: Copy block (ID)", 0),
 )
 
+removeOld = options["Move"]
+removeAir = options["Masked"]
+filtered = options["Filtered"]
+if filtered:
+    blockToCopy = options["IF Filtered: Copy block (ID)"]
+
 def perform(level, box, options):
-    for x in xrange(box.minx, box.maxx):
-        for z in xrange(box.minz, box.maxz):
-            for y in xrange(box.miny, box.maxy):
-                if level.blockAt(x, y, z) == 0:
-                    level.setBlockAt(x, y, z, 1)
+    if filtered:
+        for x in xrange(box.minx, box.maxx):
+            for z in xrange(box.minz, box.maxz):
+                for y in xrange(box.miny, box.maxy):
+                    if not (level.blockAt(x, y, z) == 0) or not removeAir:
+                        if not (level.blockAt(x, y, z) == blockToCopy)
+                            # Add to cmd
+    else:
+        for x in xrange(box.minx, box.maxx):
+            for z in xrange(box.minz, box.maxz):
+                for y in xrange(box.miny, box.maxy):
+                    if not (level.blockAt(x, y, z) == 0) or not removeAir:
+                        # Add to cmd
+    if removeOld:
+        for x in xrange(box.minx, box.maxx):
+            for z in xrange(box.minz, box.maxz):
+                for y in xrange(box.miny, box.maxy):
+                    level.setBlockAt(x, y, z, 0)
